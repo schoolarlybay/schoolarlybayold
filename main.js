3c3586ff12fa5053c1943532ba082d874c576936
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-10-11 11:45:42 -0400

    hi

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+/*=======================================================
+Copyright
+=======================================================*/
+
 var getCurrentDate = window.setInterval(function() {
   var today = new Date();
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
   document.getElementById("currentTime").innerHTML = date
 }, 1000)
+
+/*=======================================================
+Prevent Copying
+=======================================================*/
+
+$(document).ready(function() {
+    $('body').bind('cut copy', function(e) {
+        e.preventDefault();
+      });
+ });
+
+/*=======================================================
+Prevent Right Click
+=======================================================
+
+ $(document).ready(function() {
+     $("body").on("contextmenu", function(e) {
+         return false;
+      });
+  });
+$(document).ready(function() {
+     $('body').bind('cut copy', function(e) {
+         e.preventDefault();
+      });
+  });
+*/
