424dc1b67308d2bbc56e6bbe21d23cb74977baec
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-10-23 18:02:03 -0400

    hi

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,7 +5,7 @@ Copyright
 var getCurrentDate = window.setInterval(function() {
   var today = new Date();
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-  document.getElementById("currentTime").innerHTML = date
+  document.getElementById("currentTime").innerHTML = date;
 }, 1000)
 
 /*=======================================================
